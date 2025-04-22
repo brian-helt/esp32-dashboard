d5d8eb3cc00ab40538beadac6f8d23943f8f3472
dennish-games <116136730+dennish-games@users.noreply.github.com>
2025-04-21 17:36:15 -0700

    Added Lux classification

--- app.py
+++ app.py
@@ -5,6 +5,31 @@ app = Flask(__name__)
 # Store the latest value in memory (can switch to DB later)
 latest_value = {"value": None}
 
+def classify_lux_value(lux):
+    print(type(lux))
+    if lux is None:
+        return "No Data"
+    elif lux < 10:
+        return "Nighttime"
+    elif lux < 50:
+        return "Very Dark"
+    elif lux < 100:
+        return "Dim Evening"
+    elif lux < 500:
+        return "Normal Indoor Lighting or Evening"
+    elif lux < 1000:
+        return "Bright Indoor Lighting or Overcast Day"
+    elif lux < 5000:
+        return "Shaded Outdoor Lighting"
+    elif lux < 10000:
+        return "Full Daylight, Indirect Sun"
+    elif lux < 30000:
+        return "Direct Sunlight"
+    elif lux >= 30000:
+        return "Very Bright Direct Sunlight"
+        
+
+
 @app.route("/")
 def index():
     return render_template("index.html", value=latest_value["value"])
@@ -20,7 +45,11 @@ def sensor_data():
 
 @app.route("/api/latest", methods=["GET"])
 def get_latest():
-    return jsonify(latest_value)
+    response = {
+        "value" : latest_value["value"],
+        "classification" : classify_lux_value(latest_value["value"])
+    }
+    return jsonify(response)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
